1fb273c886dbaa2e8b3dbffc9b9f43dc8ba59ecb
Misha <host.misha@googlemail.com>
2021-07-28 14:39:12 +0200

    Wert1 - - Wert2

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,10 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+let Wert1,Summe,Wert2;
+Wert1 = prompt("Bitte ersten Wert eintragen");
+Wert2 = prompt("Bitte zweiten Wert eintragen");
+Summe = (Wert1 - - Wert2)
+
+
+console.log("Die Summe der Zahlen ist:" , Summe);
